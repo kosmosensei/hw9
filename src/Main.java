0f7122f5ef4e616498ff65ac7a48f5e965b83517
kosmo <kosmosensei@gmail.com>
2023-03-20 10:23:58 +0700

    complete hw

--- src/Main.java
+++ src/Main.java
@@ -2,48 +2,50 @@ import java.time.LocalDate;
 
 public class Main {
 
-    public static boolean leapYearYesNo(int year){
-        return ((year % 4 == 0 && year !=100) || year % 400==0);
+    public static boolean isYearLeap(int year) {
+        return ((year % 4 == 0 && year != 100) || year % 400 == 0);
     }
+
     private static void leapYearSay(int year) {
         System.out.println("Task 1");
-        if (leapYearYesNo(year)){
+        if (isYearLeap(year)) {
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
         }
     }
-    private static void mobileApp(int year, int clientOS){
+
+    private static void mobileApp(int year, int clientOS) {
         System.out.println("Task 2");
-        if (clientOS == 0){
-            if (year < 2015){
+        if (clientOS == 0) {
+            if (year < 2015) {
                 System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else{
+            } else {
                 System.out.println("Установите версию приложения для iOS по ссылке");
             }
         } else {
-            if (year<2015){
+            if (year < 2015) {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            } else{
+            } else {
                 System.out.println("Установите версию приложения для Android по ссылке");
             }
         }
     }
 
-    public static void sayDeliveryDay(int deliveryDistance){
+    public static void PrintDeliveryDay(int deliveryDistance) {
         System.out.println("Задача 3");
         int deliveryDay = deliveryDistance(deliveryDistance);
-        if(deliveryDay<0){
+        if (deliveryDay < 0) {
             System.out.println("Доставки нет");
-        } else if(deliveryDay==1){
-            System.out.println("Доставка: "+deliveryDay+ " день." );
+        } else if (deliveryDay == 1) {
+            System.out.println("Доставка: " + deliveryDay + " день.");
         } else {
-            System.out.println("Доставка: "+deliveryDay+ " дня." );
+            System.out.println("Доставка: " + deliveryDay + " дня.");
         }
     }
 
-    public static int deliveryDistance (int distance){
-        if (distance<20){
+    public static int deliveryDistance(int distance) {
+        if (distance < 20) {
             return 1;
         } else if (distance < 60) {
             return 2;
@@ -58,8 +60,8 @@ public class Main {
         int yearToDay = LocalDate.now().getYear(); //Получаем текущий год
         leapYearSay(yearToDay); // Модуль високосного года.
         int num = 0;
-        mobileApp(yearToDay,num); // Модуль установки приложения.
+        mobileApp(yearToDay, num); // Модуль установки приложения.
         int distance = 10;
-        sayDeliveryDay(distance);
+        PrintDeliveryDay(distance);
     }
 }
